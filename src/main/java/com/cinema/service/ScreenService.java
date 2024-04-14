e969631f8273e931c48658c07ffc379376ea6f96
AvivBurshtein <aviv.burshtein1@gmail.com>
2024-04-14 19:19:31 +0300

    add get screens by movie api

--- src/main/java/com/cinema/service/ScreenService.java
+++ src/main/java/com/cinema/service/ScreenService.java
@@ -3,7 +3,11 @@ package com.cinema.service;
 import com.cinema.dto.ScreenDto;
 import com.cinema.exception.ServiceException;
 
+import java.util.List;
+
 public interface ScreenService {
 	
 	ScreenDto getScreen(Integer screenId) throws ServiceException;
+
+    List<ScreenDto> getScreensByMovieId(Integer movieId) throws ServiceException;
 }
