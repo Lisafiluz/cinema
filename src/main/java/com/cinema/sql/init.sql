4375ade9009399913512dd4e4572a5031d8f5830
liluz <liluz@ebay.com>
2024-05-17 17:32:59 +0300

    finishes

--- src/main/java/com/cinema/sql/init.sql
+++ src/main/java/com/cinema/sql/init.sql
@@ -9,8 +9,7 @@ CREATE TABLE users
 
 
 INSERT INTO users (user_id, username, password)
-VALUES (0, 'lisaf', 'Lisaf123'),
-       (222, 'test', 'test123');
+VALUES (0, 'lisaf', '$2a$10$x3ZrcvVWcQ23uav1RWeZAeQsxUSkwdrf4cK/o2Qr4j3KUDaFI4atG');
 
 DROP TABLE IF EXISTS roles CASCADE;
 CREATE TABLE roles
@@ -34,8 +33,7 @@ CREATE TABLE user_roles
 );
 INSERT INTO user_roles(user_id, role_id)
 VALUES (0, 1),
-       (0, 2),
-       (222, 1);
+       (0, 2);
 
 
 DROP TABLE IF EXISTS movies CASCADE;
@@ -57,16 +55,16 @@ CREATE TABLE movies
 -- GPT
 INSERT INTO movies(movie_id, title, description, release_date, genre, pic_url, trailer_url, duration, is_popular, review)
 VALUES
-    (1, 'The Odyssey', 'A thrilling adventure based on the epic poem', '2023-08-15', 'Adventure', '1/1.jpg', '1/1.mp4', 150, TRUE, 92),
-    (2, 'Laugh Riot', 'An uproarious comedy that will leave you in stitches', '2024-01-25', 'Comedy', '2/2.jpg', '2/2.mp4', 110, TRUE, 88),
-    (3, 'Love Eternal', 'A timeless romance set against the backdrop of a war-torn city', '2023-05-10', 'Romance', '3/3.jpg', '3/3.mp4', 125, TRUE, 95),
-    (4, 'Nightmare House', 'A terrifying horror film that will haunt your dreams', '2024-10-31', 'Horror', '4/4.jpg', '4/4.mp4', 105, TRUE, 85),
-    (5, 'Cosmic Odyssey', 'Embark on a journey through the cosmos in this mind-bending sci-fi epic', '2023-12-20', 'Sci-Fi', '5/5.jpg', '5/5.mp4', 140, TRUE, 90),
-    (6, 'Murder Mystery', 'A gripping mystery that will keep you guessing until the very end', '2024-07-05', 'Mystery', '6/6.jpg', '6/6.mp4', 120, TRUE, 88),
-    (7, 'Action Legends', 'Thrilling action sequences and pulse-pounding excitement await in this adrenaline-fueled spectacle', '2024-03-08', 'Action', '7/7.jpg', '7/7.mp4', 130, TRUE, 90),
-    (8, 'Realm of Fantasy', 'Journey to a fantastical world filled with magic and wonder', '2023-09-01', 'Fantasy', '8/8.jpg', '8/8.mp4', 135, TRUE, 92),
-    (9, 'Heartfelt Drama', 'An emotional drama that explores the complexities of human relationships', '2024-06-15', 'Drama', '9/9.jpg', '9/9.mp4', 145, TRUE, 86),
-    (10, 'Earth Chronicles', 'A captivating documentary that delves into the history of our planet', '2023-04-12', 'Documentary', '10/10.jpg', '10/10.mp4', 120, TRUE, 94);
+    (1, 'The Odyssey', 'A thrilling adventure based on the epic poem', '2023-08-15', 'Adventure', '1/1.jpeg', '1/1.mp4', 150, TRUE, 92),
+    (2, 'Laugh Riot', 'An uproarious comedy that will leave you in stitches', '2024-01-25', 'Comedy', '2/2.jpeg', '2/2.mp4', 110, TRUE, 88),
+    (3, 'Love Eternal', 'A timeless romance set against the backdrop of a war-torn city', '2023-05-10', 'Romance', '3/3.jpeg', '3/3.mp4', 125, TRUE, 95),
+    (4, 'Nightmare House', 'A terrifying horror film that will haunt your dreams', '2024-10-31', 'Horror', '4/4.jpeg', '4/4.mp4', 105, TRUE, 85),
+    (5, 'Cosmic Odyssey', 'Embark on a journey through the cosmos in this mind-bending sci-fi epic', '2023-12-20', 'Sci-Fi', '5/5.jpeg', '5/5.mp4', 140, TRUE, 90),
+    (6, 'Murder Mystery', 'A gripping mystery that will keep you guessing until the very end', '2024-07-05', 'Mystery', '6/6.jpeg', '6/6.mp4', 120, TRUE, 88),
+    (7, 'Action Legends', 'Thrilling action sequences and pulse-pounding excitement await in this adrenaline-fueled spectacle', '2024-03-08', 'Action', '7/7.jpeg', '7/7.mp4', 130, TRUE, 90),
+    (8, 'Realm of Fantasy', 'Journey to a fantastical world filled with magic and wonder', '2023-09-01', 'Fantasy', '8/8.jpeg', '8/8.mp4', 135, TRUE, 92),
+    (9, 'Heartfelt Drama', 'An emotional drama that explores the complexities of human relationships', '2024-06-15', 'Drama', '9/9.jpeg', '9/9.mp4', 145, TRUE, 86),
+    (10, 'Earth Chronicles', 'A captivating documentary that delves into the history of our planet', '2023-04-12', 'Documentary', '10/10.jpeg', '10/10.mp4', 120, TRUE, 94);
 
 
 
@@ -116,19 +114,6 @@ FROM
     (SELECT movie_id FROM movies) AS m
         CROSS JOIN
     generate_series(1, 3);
-
-INSERT INTO screens (screen_id, movie_id, hall_id, timestamp)
-VALUES (1, 2, 1, '2024-03-20 12:00:00'),
-       (2, 1, 2, '2024-03-21 15:30:00'),
-       (3, 3, 3, '2024-03-22 18:45:00'),
-       (4, 4, 4, '2024-03-23 21:00:00'),
-       (5, 8, 5, '2024-03-24 13:20:00'),
-       (6, 3, 1, '2024-03-25 16:10:00'),
-       (7, 4, 2, '2024-03-26 19:25:00'),
-       (8, 5, 3, '2024-03-27 22:30:00'),
-       (9, 8, 4, '2024-03-28 14:40:00'),
-       (10, 3, 5, '2024-03-29 17:55:00');
-
 
 DROP TABLE IF EXISTS seats CASCADE;
 CREATE TABLE seats
